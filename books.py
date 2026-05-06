04b3a2b583b7694cf56146f1f733a5e2a4d632c6
emmanuellawilson07-arch <emmanuellawilson07@gmail.com>
2026-05-06 12:38:11 +0100

    Implement book management functions
    Added functions to manage books: add, list, search, and delete.

--- books.py
+++ books.py
@@ -1,1 +1,28 @@
+def ajouter_livre(titre, auteur, isbn=""):
+    data = charger_donnees()
+    livre = {
+        "titre": titre,
+        "auteur": auteur,
+        "isbn": isbn,
+        "disponible": True
+    }
+    data["livres"].append(livre)
+    sauvegarder_donnees(data)
+    return livre
 
+def lister_livres():
+    data = charger_donnees()
+    return data["livres"]
+
+def rechercher_livre(terme):
+    data = charger_donnees()
+    resultats = []
+    for livre in data["livres"]:
+        if terme.lower() in livre["titre"].lower() or terme.lower() in livre["auteur"].lower():
+            resultats.append(livre)
+    return resultats
+
+def supprimer_livre(titre):
+    data = charger_donnees()
+    data["livres"] = [l for l in data["livres"] if l["titre"] != titre]
+    sauvegarder_donnees(data)
